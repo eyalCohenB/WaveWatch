c9ffe6d1057bb420d725ac75d2d100780a708a51
Eyal Cohen <eyalaxl135@gmail.com>
2024-08-22 19:44:32 +0300

    minor loop change

--- screenShot_auto.py
+++ screenShot_auto.py
@@ -2,9 +2,28 @@ import pyautogui
 import os
 import time
 
+
+def takeScreenShots(dir_name,interval):
+    try:
+        i = 1
+        start_time = time.time()
+        while time.time() - start_time < 300: # working for 5 min (60sec = min)
+            img_name = "img"+ str(i) + ".png"
+            img_path = dir_name + "/" + img_name
+            image = pyautogui.screenshot(img_path)
+            print("screenshot" + str(i))
+            i+=1
+            time.sleep(interval) # take screenie every interval.
+
+    except KeyboardInterrupt:
+        print("Finished Taking screenies at: " + dir_name)
+        pass
+
+
+
 def main():
 
-    interval = 2 # 2 sec
+    interval = 1 # Seconds
     dir_name = "wave_pics"
 
     # Create a directory for screenies
@@ -16,23 +35,9 @@ def main():
 
     # Sleep to wait for screen change to waves video
     time.sleep(3) # 3 sec
-
     # Start screen capture loop.
-    try:
-        i = 1
-        start_time = time.time()
-        while time.time() - start_time < 300: # working for 5 min (60sec = min)
-            img_name = "img"+ str(i) + ".png"
-            img_path = dir_name + "/" + img_name
-            image = pyautogui.screenshot(img_path)
-            print("screenshot" + str(i))
-            i+=1
-            time.sleep(interval) # take screenie every interval secs.
-
-    except KeyboardInterrupt:
-        print("Finished Taking screenies at: " + dir_name)
-        pass
-
+    takeScreenShots(dir_name,interval)
+    
     
 if __name__ == "__main__":
     main()
